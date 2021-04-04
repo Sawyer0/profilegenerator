616163aef35bd6ca2c0a93aaf111b3778875b3f8
Dawan Sawyer <dawansawyer@gmail.com>
2021-04-03 20:50:56 -0400

    Added functions to add more teammates and functions to push the data

--- index.js
+++ index.js
@@ -1,110 +1,158 @@
-const inquirer = require('inquirer');
-
+const inquirer = require("inquirer");
+let teammateProfile = [];
 
 // --------- prompt for info
 // input for team manager’s name, employee ID, email address, and office number
 
 const promptUser = () => {
-    return inquirer.prompt([
+  return inquirer
+    .prompt([
       {
-        type: 'input',
-        name: 'team managers name',
-        message: 'Enter a manager’s name.',
+        type: "input",
+        name: "team managers name",
+        message: "Enter a manager’s name.",
       },
       {
-        type: 'number',
-        name: 'employee ID',
-        message: 'Enter an employee ID.',
+        type: "number",
+        name: "employee ID",
+        message: "Enter an employee ID.",
       },
       {
-        type: 'input',
-        name: 'email address',
-        message: 'Enter an email address.',
+        type: "input",
+        name: "email address",
+        message: "Enter an email address.",
       },
       {
-        type: 'number',
-        name: 'office number',
-        message: 'Enter an office number.',
+        type: "number",
+        name: "office number",
+        message: "Enter an office number.",
       },
-    // list for add an engineer or an intern or to finish building my team
+      // list for add an engineer or an intern or to finish building my team
       {
-        type: 'list',
-        name: 'final prompt',
-        message: 'Add an engineer or an intern or to finish building your team.',
-        choices: ['engineer', 'intern', 'nobody'],
-        default: 'nobody'
-    }
-  ])
+        type: "list",
+        name: "final prompt",
+        message:
+          "Add an engineer or an intern or to finish building your team.",
+        choices: ["Engineer", "Intern", "Nobody"],
+        default: "Nobody",
+      },
+    ])
 
-  .then(function (data) {
+    .then(function (data) {
+      switch (data.addTeammates) {
+        case "Yes, an engineer.":
+          addEngineer();
+          break;
 
-    switch (data.addTeammates) {
-        case "Yes, add an engineer":
-            addEngineer();
-            break;
-
-        case "Yes, add an intern":
-            addIntern();
-            break;
-        case "No, my team is complete":
+        case "Yes, an intern.":
+          addIntern();
+          break;
+        case "No, I am finished.":
           finishProfile();
-            break;
-    }
-});
-}
+          break;
+      }
+    });
+};
 
 function addEngineer() {
-    inquirer.prompt([
-        {
+  inquirer
+    .prompt([
+      {
         type: "input",
         message: "Enter engineers name.",
-        name: "name"
-    },
-    {
+        name: "name",
+      },
+      {
         type: "number",
         message: "Enter engineers ID number.",
-        name: "id"
-    },
-    {   
+        name: "id",
+      },
+      {
         type: "input",
         message: "Enter engineers email address.",
-        name: "email"
-    },
-    {   
+        name: "email",
+      },
+      {
         type: "input",
         message: "Enter engineers GitHub username",
-        name: "github"
-    },
-])
+        name: "github",
+      },
+    ])
+    .then(function (data) {
+      const name = data.name;
+      const id = data.id;
+      const email = data.email;
+      const github = data.github;
+      const teammate = new Engineer(name, id, email, github);
+      teammateProfile.push(teammate);
+      addTeammates();
+    });
 };
 
 function addIntern() {
-    inquirer.prompt([
-        {
+  inquirer
+    .prompt([
+      {
         type: "input",
         message: "Enter interns name.",
-        name: "name"
-    },
-    {
+        name: "name",
+      },
+      {
         type: "number",
         message: "Enter engineers ID number.",
-        name: "id"
-    },
-    {
+        name: "id",
+      },
+      {
         type: "input",
         message: "Enter engineers email address.",
-        name: "email"
-    },
-    {   
+        name: "email",
+      },
+      {
         type: "input",
         message: "Enter school for intern.",
-        name: "school"
-    },
-])
+        name: "school",
+      },
+    ])
+    .then(function (data) {
+      const name = data.name;
+      const id = data.id;
+      const email = data.email;
+      const school = data.school;
+      const teammate = new Intern(name, id, email, school);
+      teammateProfile.push(teammate);
+      addTeammates();
+    });
+};
+
+function addTeammates() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Add more teammates?",
+        choices: ["Yes, an engineer.", "Yes, an intern.", "No, I am finished."],
+        name: "addMoreTeammates",
+      },
+    ])
+
+    .then(function (data) {
+      switch (data.addMoreTeammates) {
+        case "Yes, an engineer.":
+          addEngineer();
+          break;
+
+        case "Yes, an intern.":
+          addIntern();
+          break;
+        case "No, I am finished.":
+          finishProfile();
+          break;
+      }
+    });
 };
 // WHEN I select the engineer option
 // THEN I am prompted with enter the engineer’s name, ID, email, and GitHub username,
 
-//    
+//
 
 // --------- output to html
